998450b5da798f2f6e412395f59177b4df0a509c
Dmitriy Zombak <zombak@gmail.com>
2020-02-20 17:29:01 +0300

    Final commit

--- csvtocue.py
+++ csvtocue.py
@@ -2,49 +2,65 @@ import sys
 import os
 from os import path
 
-# Настройки экспорта
+# Export Settings
 inputfile = "markers.csv"
 outputfile = "markers.cue"
 podcastgenre = "Podcast"
+podcastcomment = "Created by CSVtoCUE by Dmitriy Zombak (Zavtracast)"
 podcastperformer = "Podcast Name"
 podcastdate = "2020"
 podcasttitle = "Podcast Episode Title"
 podcastfilename = "filename.mp3"
 
-# Открываем файл на чтение
-if path.exists(inputfile):
-    print("Открываем файл", inputfile, "на чтение")
+########################################################################################################################
+#                              DO NOT TOUCH EVERYTHING BELOW THAT LINE
+########################################################################################################################
 
-    # С открытым файлом делаем следующее
+# Open file
+if path.exists(inputfile):
+    print("Open file", inputfile, "to read")
+
+    # With the open file we do this
     with open(inputfile) as f:
-        # Читаем его по строчкам (отбрасываем символ новой строки)
+        # Read it line by line (and remove first symbol)
         array = [line.rstrip('\n') for line in f]
 
-        # Выкидываем первую строку массива целиком (всё равно там ерунда)
+        # Delete first row (there is no need for this row)
         del array[0]
 
-        # Подсчитываем получившее количество строчек
+        # Count lines in file
         length = len(array)
-        print("Всего строк в файле: ", length, "\n\n")
+        print("Found", length, "lines\n")
 
-        # Построчно делаем массив для каждой строки
-        for oneline in array:
-            # Разделяем по табам в массив
-            newline = oneline.split("\t")
-
-            # Отбрасываем миллисекунды (они не нужны)
-            timearray = newline[1].split(".")
-            time = timearray[0]
-            chaptername = newline[0]
-
-            print("Глава %d:", chaptername, "время:", time)
-
-        # Пока у нас открыт файл на чтение, мы можем открыть файл на запись
+        # When we open a reading file, we also need a writing file
         with open(outputfile, 'w') as filetowrite:
-            print("\n\nОткрываем файл", outputfile, "на запись")
-            content = print("REM GENRE", podcastgenre, "\n")
-            filetowrite.write(content)
+            # Let's put a headers down there (tabs and spaces are important here!)
+            firstcontent = """REM GENRE %s
+REM DATE %s
+REM COMMENT \"%s\"
+PERFORMER \"%s\"
+TITLE \"%s\"
+FILE \"%s\"""" % (podcastgenre, podcastdate, podcastcomment, podcastperformer, podcasttitle, podcastfilename)
+            filetowrite.write(firstcontent)
 
+            # We created a list for every single line
+            for linenumber, oneline in enumerate(array):
+                # Split it by tabs
+                newline = oneline.split("\t")
+                # Get rid if milliseconds (no need of them) and convert time
+                timearray = newline[1].split(".")
+                time = timearray[0]
+                # Oh, there is a fine chapter name
+                chaptername = newline[0]
+                # Get some linenumber and if it below 10, to format it with leading zero
+                linenumber = linenumber+1
+                if(linenumber<10):
+                    linenumber = '%0*d' % (2, linenumber)
+                # Let's write chapters in file (tabs and spacec are important here!)
+                maincontent = """\n    TRACK %s AUDIO\n        TITLE \"%s\"\n        PERFORMER \"%s\"\n        INDEX 01 %s""" % (linenumber, chaptername, podcastperformer, time)
+                filetowrite.write(maincontent)
+
+        print("Everything done. Results are in output file", outputfile, "\n")
 
 else:
-    print("Файл", inputfile, "не найден!")
+    print("File", inputfile, "not found! Please double check the export settings!\n")
